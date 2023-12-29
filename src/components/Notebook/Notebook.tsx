c6e2bd7188e82e67eece75b58134899df4fd8273
Bill McDavitt <wmcdavitt@gmail.com>
2023-12-29 10:39:11 -0500

    Add Notebook Hierarchy View and updated Test Data File

--- src/components/Notebook/Notebook.tsx
+++ src/components/Notebook/Notebook.tsx
@@ -1,19 +1,51 @@
 import { useParams } from "react-router-dom";
-import { NotebooksData } from "../data";
+import { NotebookChapters, INotebookChapter } from "../data";
+import Tree, { DataNode } from "antd/es/tree";
 
 const Notebook = () => {
-  const getNotebookForId = (id?: number) => {
-    const found = NotebooksData.find((Notebook) => {
-      return Notebook.id === id;
-    });
-    return found;
+  const getChaptersForNotebook = (notebookId?: number) => {
+    const chapters = NotebookChapters.filter((chapter) => chapter.notebookId === notebookId);
+    return chapters;
   };
 
   const { notebookId } = useParams();
 
-  const Notebook = getNotebookForId(notebookId ? parseInt(notebookId) : undefined);
+  const Chapters = getChaptersForNotebook(notebookId ? parseInt(notebookId) : undefined);
 
-  return <h2>{Notebook && Notebook.title} </h2>;
+  // Function to generate tree data recursively
+
+  function generateTree(chapters: (INotebookChapter | undefined)[], parentId: number | null = null) {
+    const nodes = [];
+
+    for (const chapter of chapters) {
+      if (chapter && chapter.parentChapter === parentId) {
+        const node: DataNode = {
+          title: chapter.title,
+          key: `node-${chapter.id}`,
+        };
+
+        if (chapter.childChapters.length) {
+          const mappedChildChapters = chapter.childChapters.map((chapter) => {
+            return NotebookChapters.find((notebookChapter) => notebookChapter.id === chapter);
+          });
+
+          const childChapters = generateTree(mappedChildChapters, chapter.id);
+          if (childChapters.length) {
+            node.children = childChapters;
+          }
+        }
+
+        nodes.push(node);
+      }
+    }
+
+    return nodes;
+  }
+
+  // Call the function with the provided NotebookChapters
+  const chapterData: DataNode[] = generateTree(Chapters);
+
+  return <Tree treeData={chapterData} />;
 };
 
 export default Notebook;
